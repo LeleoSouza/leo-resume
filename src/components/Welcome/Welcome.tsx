0d452146c60ff87600af8701b95a8804b1a3abf8
Leonardo D’Souza <migo@mac.mynetworksettings.com>
2025-05-04 18:30:07 -0400

    Animation added to welcome page

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,30 +1,87 @@
-import { Box, Card, Stack, Typography, IconButton } from '@mui/material';
+import { Box, Card, Typography } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import ContactPageIcon from '@mui/icons-material/ContactPage';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import cover from '../../assets/welcome-wallpaper.jpg';
 import resume from '../../assets/leonardo-resume.pdf';
+// import Ballpit from '../Background/Ballpit';
+// import Threads from '../Background/Threads';
+// import Particles from '../Background/Particles';
+import Aurora from '../Background/Aurora';
+import Dock from '../Dock/Dock';
+
 export const Welcome = () => {
+  const items = [
+    {
+      icon: <GitHubIcon sx={{ color: 'white' }} />,
+      label: 'GitHub',
+      onClick: () => window.open('https://github.com/LeleoSouza'),
+    },
+    {
+      icon: <LinkedInIcon sx={{ color: 'white' }} />,
+      label: 'LinkedIn',
+      onClick: () => window.open('https://www.linkedin.com/in/leonardo-souza-490188ba/'),
+    },
+    {
+      icon: <ContactPageIcon sx={{ color: 'white' }} />,
+      label: 'Download Resume',
+      onClick: () => window.open(resume),
+    },
+  ];
   return (
     <Box
       id='welcome-page-anchor'
       sx={{
+        position: 'relative',
         height: '100vh',
         overflowY: 'auto',
         flexWrap: 'wrap',
         display: 'flex',
         alignContent: 'center',
         justifyContent: 'center',
-        backgroundImage: `url(${cover})`,
         backgroundSize: 'cover',
+        backgroundColor: 'black',
+        overflow: 'hidden',
       }}
     >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: 0,
+        }}
+      >
+        {/* <Threads amplitude={1} distance={0} enableMouseInteraction={true} /> */}
+        {/* <Ballpit
+          count={90}
+          gravity={0.5}
+          friction={0.9975}
+          wallBounce={0.95}
+          followCursor={false}
+        /> */}
+        {/* <Particles
+          particleColors={['#ffffff', '#ffffff']}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={300}
+          moveParticlesOnHover={false}
+          alphaParticles={false}
+          disableRotation={false}
+        /> */}
+        import Aurora from './Aurora';
+        <Aurora colorStops={['#F5F5DC', '#4B6A9B', '#F5F5DC']} blend={0.5} amplitude={2.0} speed={0.5} />
+      </Box>
       <Card
         sx={{
           width: { xs: '95%', md: '70%', lg: '60%' },
+          position: 'relative',
+          zIndex: 1, // above the Threads
           padding: '30px',
           borderRadius: '6px',
-          backgroundColor: 'rgba(13, 13, 13, 0.57)',
+          backgroundColor: 'rgba(31, 31, 31, 0.57)',
           color: 'white',
           display: 'flex',
           flexWrap: 'wrap',
@@ -38,27 +95,8 @@ export const Welcome = () => {
           enterprise-level applications using the latest technologies. I'm always excited about creating software that
           makes a difference and delivering seamless, intuitive experiences.
         </Typography>
-        <Box sx={{ justifyContent: 'center' }}>
-          <Stack direction='row' spacing={3}>
-            <IconButton onClick={() => window.open('https://github.com/LeleoSouza')}>
-              <GitHubIcon sx={{ color: 'white' }} />
-            </IconButton>
-            <IconButton
-              sx={{ color: 'white' }}
-              onClick={() => {
-                window.open('https://www.linkedin.com/in/leonardo-souza-490188ba/');
-              }}
-            >
-              <LinkedInIcon />
-            </IconButton>
-            <IconButton
-              onClick={() => {
-                window.open(resume);
-              }}
-            >
-              <ContactPageIcon sx={{ color: 'white' }} />
-            </IconButton>
-          </Stack>
+        <Box sx={{ height: '50px' }}>
+          <Dock items={items} panelHeight={68} baseItemSize={50} magnification={70} />
         </Box>
       </Card>
     </Box>
